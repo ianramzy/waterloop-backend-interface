8eb53b7eb4e506c0174ddeb395db464fc50f601b
ianramzy <ianramzy@gmail.com>
2019-03-16 16:55:59 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,163 @@
+// using mgo package - https://github.com/globalsign/mgo
+// Tests - https://github.com/globalsign/mgo/blob/master/session_test.go
+package main
+
+import (
+	"fmt"
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+	"log"
+)
+
+const (
+	url = "localhost"
+)
+
+type SensorData struct {
+	ID        bson.ObjectId `bson:"_id,omitempty"`
+	SensorID  uint32
+	Data      uint64
+	Timestamp uint64
+}
+
+type CommandData struct {
+	ID        bson.ObjectId `bson:"_id,omitempty"`
+	CommandID uint32
+	Data      uint64
+	Timestamp uint64
+}
+
+func main() {
+	session, err := mgo.Dial(url)
+	if err != nil {
+		fmt.Printf("couldnt connect to DB :(")
+		log.Fatal(err)
+	}
+	defer session.Close()
+	fmt.Printf("Successfully connected to mongodb server at %v\n", url)
+
+	dbName := "test"
+	db := session.DB(dbName)
+	if db == nil {
+		fmt.Printf("db '%v' not found, exiting...", dbName)
+		return
+	}
+
+	// define locations of collections, just two for now: sensor and command
+	collectionS := session.DB("test").C("sensor")
+	collectionC := session.DB("test").C("command")
+
+	testSD := SensorData{SensorID: 001, Data: 99, Timestamp: 123456789}
+	testCD := CommandData{CommandID: 005, Data: 1234, Timestamp: 123456789}
+
+	insertSensorItem(testSD, collectionS)
+	insertCommandItem(testCD, collectionC)
+
+	//err = collectionS.Insert(&SensorData{SensorID: 001, Data: 99, Timestamp: 123456789})
+	//if err != nil {fmt.Printf("Error inserting sensor item")}
+
+	fmt.Printf("All done!")
+}
+
+func insertSensorItem(newItem SensorData, collection *mgo.Collection) {
+	err := collection.Insert(&SensorData{SensorID: newItem.SensorID, Data: newItem.Data, Timestamp: newItem.Timestamp})
+
+	if err != nil {
+		fmt.Printf("Error inserting sensor item")
+	} else {
+		fmt.Printf("Sensor item Inserted")
+	}
+}
+
+func insertCommandItem(newItem CommandData, collection *mgo.Collection) {
+	err := collection.Insert(&SensorData{SensorID: newItem.CommandID, Data: newItem.Data, Timestamp: newItem.Timestamp})
+
+	if err != nil {
+		fmt.Printf("Error inserting command item")
+	} else {
+		fmt.Printf("command item Inserted")
+	}
+}
+
+//func listDocs(db *mgo.Database, col string) {
+//
+//	coll := db.C(col)
+//
+//	if coll == nil {
+//		return
+//	}
+//
+//	type Document struct {
+//		ID   int    `json:"_id,omitempty"`
+//		Desc string `json:"desc,omitempty"`
+//		Done bool   `json:"done,omitempty"`
+//	}
+//
+//	var result []map[string]string // see bson.M
+//
+//	coll.Find(nil).All(&result)
+//
+//	for i, d := range result {
+//		fmt.Printf("\tDoc%3v - %#v\n", i+1, d)
+//		obj := bson.ObjectId(d["_id"])
+//		fmt.Printf("\t\tHex: %v, String: %v, Time: %v\n", obj.Hex(), obj.String(), obj.Time())
+//	}
+//}
+
+//func main() {
+//	dbName := "test"
+//	if 1 == len(os.Args) {
+//		log.Warnf("No db specified, using '%v'", dbName)
+//	} else {
+//		dbName = os.Args[1]
+//	}
+//	// list documents in selected database collections
+//	// ----
+//	session, err := mgo.Dial(url)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	defer session.Close()
+//
+//	log.Infof("Successfully connected to mongodb server at %v", url)
+//
+//	db := session.DB(dbName)
+//	if db == nil {
+//		log.Errorf("db '%v' not found, exiting...", dbName)
+//		return
+//	}
+//
+//	// iterate collections
+//	fmt.Printf("Collections in db '%v':\n", dbName)
+//	cols, err := db.CollectionNames()
+//	if err != nil {
+//		return
+//	}
+//
+//	for _, c := range cols {
+//		fmt.Printf("[%v]\n", c)
+//		listDocs(db, c)
+//	}
+//}
+//
+//func listDocs(db *mgo.Database, col string) {
+//	coll := db.C(col)
+//	if coll == nil {
+//		return
+//	}
+//
+//	type Document struct {
+//		ID   int    `json:"_id,omitempty"`
+//		Desc string `json:"desc,omitempty"`
+//		Done bool   `json:"done,omitempty"`
+//	}
+//
+//	var result []map[string]string // see bson.M
+//	coll.Find(nil).All(&result)
+//
+//	for i, d := range result {
+//		fmt.Printf("\tDoc%3v - %#v\n", i+1, d)
+//		obj := bson.ObjectId(d["_id"])
+//		fmt.Printf("\t\tHex: %v, String: %v, Time: %v\n", obj.Hex(), obj.String(), obj.Time())
+//	}
+//}
